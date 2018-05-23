a43dc37f661e4b0d4598f7b5372ed580a4750c97
fNedopetalski <37910959+fNedopetalski@users.noreply.github.com>
2018-05-22 21:46:31 -0300

    Versão 3.1 (4.4)
    Alteração lógica mais fácil

--- app/src/main/java/com/carro/robo/mobile/ufj/approbo/ProjetoRoboticaLogica.java
+++ app/src/main/java/com/carro/robo/mobile/ufj/approbo/ProjetoRoboticaLogica.java
@@ -23,10 +23,8 @@ public class ProjetoRoboticaLogica extends AppCompatActivity{
         return acionarEquacao;
     }
 
-    public void andaCarrinho() { // funcao que manda o carrinho andar
-        TAM = 500; // tamanho da pista em cm
-        dist = (float) (TAM / 11.8);
-        byte[] data = Float.toString(dist).getBytes();
+    public void andaCarrinho(float d) { // funcao que manda o carrinho andar
+        byte[] data = Float.toString(d).getBytes();
         ExtensorDeAplicacoes.connect.write(data);
 
 //        sendMessage(Float.toString(dist));// distancia recebe o tamanho da pista dividido pela constante 11,8
@@ -55,8 +53,10 @@ public class ProjetoRoboticaLogica extends AppCompatActivity{
     }
 
     public void logica(boolean anda) { // logica principal
+        TAM = 500; // tamanho da pista em cm
+        dist = (float) (TAM / 23.6);
         if (acionarEquacao == true) { // se o botao de start for apertado a logica entra em ação
-            if ((nivel1 == 0 || nivel1 == 1) && nivel2 == 0) {// se estiver no nivel 1 segue as instruções
+            if ((nivel1 >=0 %% nivel1 <= 4) && nivel2 == 0) {// se estiver no nivel 1 segue as instruções
                 randomNumber1 = (int) (10 * Math.random()) + 1; // primeiro random recebe um numero random de 0 a 10
                 randomNumber2 = (int) (10 * Math.random()) + 1; // segundo random recebe um numero random de 0 a 10
                 if (randomNumber1 > randomNumber2) { // se o primeiro random for maior que o segundo..
@@ -75,12 +75,12 @@ public class ProjetoRoboticaLogica extends AppCompatActivity{
                 } else { // caso ele tenha acertado o botao correto
                     nivel1++; // nivel1 recebe +1
                     if(anda == true){
-                    andaCarrinho(); // chama a função que faz o carrinho andar
+                    andaCarrinho(dist); // chama a função que faz o carrinho andar
                     }
                 }
             }// fim nivel 1
 
-            if (nivel1 == 2 && (nivel2 == 0 || nivel2 == 1)) // se a pessoa acertou pelo menos 2 questões do nivel 1 seguidas
+            if (nivel1 == 4 && (nivel2 >= 0 && nivel2 <= 4)) // se a pessoa acertou pelo menos 2 questões do nivel 1 seguidas
             {
                 max = 50; // as questões passam a ser com números de 20 à 50
                 min = 20;
@@ -102,12 +102,12 @@ public class ProjetoRoboticaLogica extends AppCompatActivity{
                 } else {
                     nivel2++; // nivel 2 recebe +1 caso esteja correto
                     if(anda == true) {
-                        andaCarrinho();
+                        andaCarrinho(dist*2);
                     }
                 }
             }// fim nivel 2
 
-            if (nivel1 == 2 && nivel2 == 2)// se a pessoa acertou pelo menos 2 questões de nivell 1 e 2 de nível 2
+            if (nivel1 == 4 && nivel2 == 4)// se a pessoa acertou pelo menos 2 questões de nivell 1 e 2 de nível 2
             {
                 max = 100; // range das questões passam a ser de 50 à 100
                 min = 50;
@@ -129,7 +129,7 @@ public class ProjetoRoboticaLogica extends AppCompatActivity{
                     nivel2--; // de nível 3, ele volta para o nivel 2;
                 } else { // não há variavel para nivel 3 pois a brincadeira esta prestes a acabar em 2 acertos no maximo
                     if(anda == true) {
-                        andaCarrinho();
+                        andaCarrinho(dist*3);
                     }
                 }
             }//nivel 3
